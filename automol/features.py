e40fb7895f9105ee8f1c20ba4ea47a1b18f961d7
Felix <spma2@gmx.net>
2020-12-06 10:37:27 +0100

    implement fingerprint generation

--- automol/features.py
+++ automol/features.py
@@ -10,14 +10,32 @@ from rdkit import Chem
 
 class FeatureGenerator:
 
+    __featureList = {
+        'fingerprint': [
+            ['smiles'],
+            lambda df:[Chem.RDKFingerprint(Chem.MolFromSmiles(smi)) for smi in df['smiles']]
+        ],
+    }
+
     def __init__(self, data_set:Dataset):
         self.data_set = data_set
         self.__generated_features = {}
 
     def get_feature(self, feature_name):
-        1
+        if feature_name not in FeatureGenerator.__featureList:
+            raise Exception('unknown features')
+
+        if FeatureGenerator.__featureList[feature_name][0] not in self.data_set:
+            raise Exception('requirements for feature %s not satisfied by data set'%feature_name)
+
+        if feature_name not in self.__generated_features:
+            self.__generated_features[feature_name] = numpy.array(
+                FeatureGenerator.__featureList[feature_name][1](self.data_set))
+
+        return self.__generated_features[feature_name]
 
 
+"""
 class Fingerprint:
 
     @staticmethod
@@ -27,3 +45,4 @@ class Fingerprint:
     @staticmethod
     def produce(data):
         return Chem.RDKFingerprint(Chem.MolFromSmiles(data['smiles']))
+"""
